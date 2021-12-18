a63760d96c8ae1832fa99d6f1f8e04366ac87bc5
Michał <Sylwek512@interia.eu>
2021-12-18 20:20:30 +0100

    koniec zad 6

--- src/Main.java
+++ src/Main.java
@@ -3,18 +3,22 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        if (23%3 == 0)
-        {
-            System.out.println("podzielne przez 3");
-        }
-        else
-        {
-            System.out.println("niepodzielne przez 3");
-        }
+        double[] tablica = {5,10,15,20,25};
 
-        int zmienna = 54159;
-        if( zmienna % 2 == 0 ) System.out.println("Podana liczba jest parzysta.");
-        else System.out.println("Podana liczba nie jest parzysta.");
+
+        tablica[0] = 40;
+
+        tablica[1] = 50;
+
+        tablica[2] = 60;
+        tablica[3] = 80;
+        tablica[4] = 100;
+
+        System.out.println("Pierwszy element: " + tablica[0]);
+        System.out.println("Drugi element: " + tablica[1]);
+        System.out.println("Trzeci element: " + tablica[2]);
+        System.out.println("Czwarty element: " + tablica[3]);
+        System.out.println("Piaty element: " + tablica[4]);
     }
 
 
